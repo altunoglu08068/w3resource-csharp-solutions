240c4157e84be92b21a627d8ba8d5d99965d15e0
Kerem ALTUNOĞLU <hkaltun16@gmail.com>
2026-08-15 12:57:44 +0300

    feat: Add solution and README for 010 Specified Formula Operations

---
+++ Exercises/01-Basic-Exercises/010-Specified-Formula-Operations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SpecifiedFormulaOperations
+{
+    internal class Program
+    {
+        static int InputNumber(string prompt)
+        {
+            int number;
+            Console.Write(prompt);
+
+            while (!int.TryParse(Console.ReadLine() ?? "", out number))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("⚠️ You entered an invalid number! Please try again.");
+                Console.ResetColor();
+                Console.Write(prompt);
+            }
+
+            return number;
+        }
+    }
+}
---
+++ Exercises/01-Basic-Exercises/010-Specified-Formula-Operations/README.md
@@ -0,0 +1,24 @@
+# 010. Specified Formula Operations
+
+**Kaynak:** [w3resource - C# Basic Exercises #10](https://www.w3resource.com/csharp-exercises/basic/csharp-basic-exercise-10.php)  
+**Konu:** Basic Exercises  
+
+---
+
+## 📝 Soru Açıklaması
+
+Kullanıcıdan alınan 3 sayı ($x, y, z$) için aşağıdaki formülleri hesaplayıp ekrana yazdıran bir C# programı yazın:
+1. $(x + y) \cdot z$
+2. $x \cdot y + y \cdot z$
+
+---
+
+## 💡 Örnek Çıktı
+
+```text
+Enter the value for x: 5
+Enter the value for y: 6
+Enter the value for z: 7
+
+Result of Formula 1 ((x + y) x z): 77
+Result of Formula 2 (xy + yz): 72
